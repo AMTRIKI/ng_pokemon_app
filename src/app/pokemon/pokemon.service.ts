29c48827959e221a80fee0c966033bb10dd464f1
Amel TRIKI <amel.triki@gmail.com>
2022-09-03 18:29:27 +0200

    Introduction des observables dans le service mais à améliorer

--- src/app/pokemon/pokemon.service.ts
+++ src/app/pokemon/pokemon.service.ts
@@ -1,14 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Observable, of, tap } from 'rxjs';
 import { POKEMONS } from './mock-pokemon-list';
 import { Pokemon } from './pokemon';
 
 @Injectable()
 export class PokemonService {
-  getPokemonList(): Pokemon[]{
-    return POKEMONS;
+
+  constructor(private http:HttpClient){
+
   }
-  getPokemonById(id:number): Pokemon|undefined{
-    return POKEMONS.find(p => p.id == id);
+  getPokemonList(): Observable<Pokemon[]>{
+    return this.http.get<Pokemon[]>('api/pokemons').pipe(
+      tap((pokemonList)=>console.table(pokemonList)),
+      catchError((error=>{
+        console.log(error); 
+        return of([])
+      }))
+    );
+   // return POKEMONS;
+  }
+  getPokemonById(pokemonId:number): Observable<Pokemon|undefined>{
+    return this.http.get<Pokemon>(`api/pokemons/${pokemonId}`).pipe(
+      tap(pokemon=>console.log(pokemon)),
+      catchError((error)=>{
+        console.log(error); return of(undefined);
+      })
+    );
+  //  return POKEMONS.find(p => p.id == id);
   }
   getPokemonTypeList():string[]{
     return [
